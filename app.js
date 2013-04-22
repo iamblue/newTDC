a171e42c774884d59cafabd67b86b6d0855c0d5b
iamblue <blue.chen@kindminds.com>
2013-04-22 11:37:16 +0800

    第一次上傳nodejs

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict'
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+//var models = require(path.join(__dirname, 'models', 'index.js'));
+
+var app = express();
+
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+require(path.join(__dirname, 'routes.js'))(app);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ routes.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var path = require('path');
+//var controllers = require(path.join(__dirname, 'controllers', 'index.js'));
+//var models = require(path.join(__dirname, 'models', 'index.js'));
+
+module.exports = function (app) {
+  app.get('/',function  (req,res){
+    res.render('index')
+
+
+
+  })
+}

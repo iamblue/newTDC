2436e6daa778c0bb3703a71625074e9b155c3986
iamblue <blue.chen@kindminds.com>
2013-05-30 17:41:44 +0800

    update new algo

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 3009);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
